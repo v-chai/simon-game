0ba205a13217d3907e3b0806ce3733280ed2c798
V Prentice <vcoprentice@gmail.com>
2021-10-20 13:25:48 -0700

    Initial commit

---
+++ lib/simon.rb
@@ -0,0 +1,43 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+
+  end
+
+  def take_turn
+
+  end
+
+  def show_sequence
+
+  end
+
+  def require_sequence
+
+  end
+
+  def add_random_color
+
+  end
+
+  def round_success_message
+
+  end
+
+  def game_over_message
+
+  end
+
+  def reset_game
+
+  end
+end
