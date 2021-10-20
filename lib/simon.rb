8c7c9cde34413977f546e408cfd98d12d7fe0724
V Prentice <vcoprentice@gmail.com>
2021-10-20 13:44:46 -0700

    Finalized methods

--- lib/simon.rb
+++ lib/simon.rb
@@ -1,3 +1,4 @@
+
 class Simon
   COLORS = %w(red blue green yellow)
 
@@ -10,18 +11,40 @@ class Simon
   end
 
   def play
-
+    until @game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    unless @game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
+    @seq.each do |color|
+      puts color
+      sleep 0.5
+      system("clear")
+    end
   end
 
   def require_sequence
+    test_seq = @sequence.dup
+    while test_seq.length > 0
+      guess = gets.chomp
+      if guess != test_seq.shift
+        @game_over = true
+        return @game_over
+      end
+    end
 
   end
 
@@ -30,14 +53,16 @@ class Simon
   end
 
   def round_success_message
-
+    puts "You got it!"
   end
 
   def game_over_message
-
+    puts "Err! You got the sequence wrong"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
